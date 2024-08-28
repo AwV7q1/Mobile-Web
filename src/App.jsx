dd035bc897417aa17545f513aa71dfad1abf33e7
Nguyen Hoang Quan 2 <nguyenhoangquan2@luvina.net>
2024-08-28 10:38:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div id='app' className='center-element'>
+            <div className='container'>
+                <div className="header center-element">
+                    header
+                </div>
+                <div className="body center-element">
+                    body
+                </div>
+                <div className="nav center-element">
+                    navigate
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
